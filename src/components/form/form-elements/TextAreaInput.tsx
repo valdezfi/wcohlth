ddd4a16b2f73b4e8d0100e18b3ce00483d5aaaa1
valdezfi <joswellv@outlook.com>
2025-07-09 22:12:13 -0400

    text

--- src/components/form/form-elements/TextAreaInput.tsx
+++ src/components/form/form-elements/TextAreaInput.tsx
@@ -10,32 +10,29 @@ export default function TextAreaInput() {
   return (
     <ComponentCard title="Textarea input field">
       <div className="space-y-6">
-        {/* Default TextArea */}
         <div>
           <Label>Description</Label>
-          <TextArea
-            value={message}
-            onChange={(value) => setMessage(value)}
-            rows={6}
-          />
+         <TextArea
+  value={message}
+  onChange={(e) => setMessage(e.target.value)}
+  rows={6}
+/>
         </div>
 
-        {/* Disabled TextArea */}
         <div>
           <Label>Description</Label>
           <TextArea rows={6} disabled />
         </div>
 
-        {/* Error TextArea */}
         <div>
           <Label>Description</Label>
-          <TextArea
-            rows={6}
-            value={messageTwo}
-            error
-            onChange={(value) => setMessageTwo(value)}
-            hint="Please enter a valid message."
-          />
+      <TextArea
+  rows={6}
+  value={messageTwo}
+  error
+  onChange={(e) => setMessageTwo(e.target.value)}
+  hint="Please enter a valid message."
+/>
         </div>
       </div>
     </ComponentCard>
