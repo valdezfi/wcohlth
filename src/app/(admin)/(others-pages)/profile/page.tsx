89920098060c626866f3bc529d8f2278393b7ee3
valdezfi <joswellv@outlook.com>
2025-07-14 01:39:58 -0400

    fix

--- src/app/(admin)/(others-pages)/profile/page.tsx
+++ src/app/(admin)/(others-pages)/profile/page.tsx
@@ -1,14 +1,16 @@
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
+
 import UserMetaCard from "@/components/user-profile/UserMetaCard";
 import UserInfoCard from "@/components/user-profile/UserInfoCard";
 
 export default async function Profile() {
+  
   const session = await getServerSession();
-
-  if (!session) {
-    redirect("/signin");
-  }
+ 
+   if (!session) {
+     redirect("/signin");
+   }
 
   return (
     <div>
