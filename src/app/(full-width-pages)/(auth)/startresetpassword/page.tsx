311a9fdd899a528df67f0e3e8f5c98f7e24f2176
valdezfi <joswellv@outlook.com>
2025-07-19 14:32:03 -0400

    fixed issue

--- src/app/(full-width-pages)/(auth)/startresetpassword/page.tsx
+++ src/app/(full-width-pages)/(auth)/startresetpassword/page.tsx
@@ -1,11 +1,6 @@
 import StartResetPassword from "@/components/auth/ResetPassword";
-import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Next.js SignUp Page | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js SignUp Page TailAdmin Dashboard Template",
-  // other metadata
-};
+
 
 export default function SignUp() {
   return <StartResetPassword />;
