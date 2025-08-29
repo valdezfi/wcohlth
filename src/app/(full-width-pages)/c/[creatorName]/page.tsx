a04806f4afc75377ebde52e9aa38255cf07e9afc
valdezfi <joswellv@outlook.com>
2025-08-29 01:20:30 -0400

    open page

--- src/app/(full-width-pages)/c/[creatorName]/page.tsx
+++ src/app/(full-width-pages)/c/[creatorName]/page.tsx
@@ -1,8 +1,4 @@
-import { getServerSession } from "next-auth/next";
-import { redirect } from "next/navigation";
 import CreatorGeneralPublicProfileCard from "@/components/creators/CreatorProfile";
-
-
 
 export default async function CreatorProfilePage({
   params,
@@ -11,12 +7,11 @@ export default async function CreatorProfilePage({
 }) {
   const resolvedParams = await params;
 
-  const session = await getServerSession();
-  if (!session) redirect("/signin");
-
   return (
     <div className="min-h-screen p-6 bg-gray-100 dark:bg-black">
-      <CreatorGeneralPublicProfileCard email={decodeURIComponent(resolvedParams.creatorName)} />
+      <CreatorGeneralPublicProfileCard
+        email={decodeURIComponent(resolvedParams.creatorName)}
+      />
     </div>
   );
 }
